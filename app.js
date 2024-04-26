68b78090c89a0e1dce1faf877fcadaf1e991a9f7
Phoebe Tang <49921653+adironene@users.noreply.github.com>
2024-04-26 10:12:56 -0700

    fixed json url and added petals animation

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  *  Note: the checkboxes do not have any current functionality when checked or unchecked
  */
@@ -10,7 +9,7 @@ const addTaskButton = document.getElementById("add-task");
 
 // Function that loads JSON file for webpage to display task list
 function loadTasks() {
-    fetch('task-list/hard-coded-tasks.json')
+    fetch('task-list/hard-coded-tasks.JSON')
         .then(response => response.json())
         .then(data => {
             data.forEach(task => {
@@ -96,7 +95,7 @@ function renderTask(taskItem) {
 
 // Function that deletes a task
 function deleteTask(divItem, taskId) {
-    fetch('task-list/hard-coded-tasks.json')
+    fetch('task-list/hard-coded-tasks.JSON')
     .then(response => response.json())
         .then(data => {
             divItem.remove();
@@ -233,7 +232,7 @@ loadTasks();
 
 // Function to fetch and display tasks from JSON
 function displayTasks() {
-    fetch('task-list/hard-coded-tasks.json')
+    fetch('task-list/hard-coded-tasks.JSON')
         .then(response => response.json())
         .then(tasks => {
             const taskWidget = document.getElementById('taskWidget');
