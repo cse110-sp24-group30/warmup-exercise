d410d3a17c10fbb973872fe91c9e86f62fe454a6
Kenneth V. <103289124+kennethkietvuong@users.noreply.github.com>
2024-04-26 17:46:12 -0700

    center elements

--- app.js
+++ app.js
@@ -406,27 +406,27 @@ function randomNumberGenerator() {
 loadTasks();
 
 // Function to fetch and display tasks from JSON
-function displayTasks() {
-    fetch('task-list/hard-coded-tasks.JSON')
-        .then(response => response.json())
-        .then(tasks => {
-            const taskWidget = document.getElementById('taskWidget');
-            tasks.forEach(task => {
-                const taskDiv = document.createElement('div');
-                taskDiv.classList.add('task');
+// function displayTasks() {
+//     fetch('task-list/hard-coded-tasks.JSON')
+//         .then(response => response.json())
+//         .then(tasks => {
+//             const taskWidget = document.getElementById('taskWidget');
+//             tasks.forEach(task => {
+//                 const taskDiv = document.createElement('div');
+//                 taskDiv.classList.add('task');
 
-                taskDiv.innerHTML = `
-                    <div class="task-id">Task ID: ${task.id}</div>
-                    <div>Task: ${task.task}</div>
-                    <div class="task-completed">Completed: ${task.completed ? 'Yes' : 'No'}</div>
-                    <div class="task-priority">Priority: ${task.priority}</div>
-                    <div>Due: ${task.due}</div>
-                `;
-                taskWidget.appendChild(taskDiv);
-            });
-        })
-        .catch(error => console.error('Error fetching tasks:', error));
-}
+//                 taskDiv.innerHTML = `
+//                     <div class="task-id">Task ID: ${task.id}</div>
+//                     <div>Task: ${task.task}</div>
+//                     <div class="task-completed">Completed: ${task.completed ? 'Yes' : 'No'}</div>
+//                     <div class="task-priority">Priority: ${task.priority}</div>
+//                     <div>Due: ${task.due}</div>
+//                 `;
+//                 taskWidget.appendChild(taskDiv);
+//             });
+//         })
+//         .catch(error => console.error('Error fetching tasks:', error));
+// }
 
 function genRandomNum() {
     var numPokemon = 80;
@@ -486,4 +486,4 @@ function displayCaughtPokemon() {
     }
 }
 // Call the function to display tasks when the page loads
-displayTasks();
+//displayTasks();
